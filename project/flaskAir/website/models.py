7e1c00bca8aef0c5a8e724d39d7396ae4bc89aa2
liphiwolf <116948484+liphiwolf@users.noreply.github.com>
2023-01-24 19:22:23 +0100

    update models.py: deleted class "Role" and table "roles_users"; added "is_admin" attribute to User class; refactored relationship between user and seat
    The classes for Role and table "roles_users" is not needed to implement a one-to-many relationship: The seat will save who is sitting on it.
    Seat.user_id == None == free seat; Seat.user_id == 0 == booked by unknown user; Seat.user_id == user.id == seat booked by user with id user.id

--- project/flaskAir/website/models.py
+++ project/flaskAir/website/models.py
@@ -1,39 +1,22 @@
 from . import db  # import of our SQLAlchemy object
 from flask_login import UserMixin
-from flask_security import RoleMixin
-
-
-""" Relation between roles and users is stored in this table """
-roles_users = db.Table('roles_users',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
 
 
 class User(db.Model, UserMixin):
-    """ Define class for users that can login and book seats """
+    """ Define class for users that can log in and book seats """
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(255))
     firstname = db.Column(db.String(255))
     lastname = db.Column(db.String(255))
-    active = db.Column(db.Boolean)
-    confirmed_at = db.Column(db.DateTime)
-    roles = db.relationship(
-        'Role',
-        secondary=roles_users,
-        backref=db.backref('users', lazy='dynamic')
-    )
+    is_admin = db.Column(db.Boolean(False))
+    # backref to user: Seat.user will give the corresponding user
+    # seats = db.relationship('Seat', backref='user')
 
 
 class Seat(db.Model):
     """ Define class for Seats to be booked """
     id = db.Column(db.Integer, primary_key=True)
-    seatID = db.Column(db.String(10), unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # relation of seat instance to user
-
-
-class Role(db.Model, RoleMixin):
-    """ Define class for roles admin and user """
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40))
-    description = db.Column(db.String(255))
+    seat_name = db.Column(db.String(10), unique=True)
+    user_id = db.Column(db.Integer, default=None)  # set to 0 if booked by unknown, else to user.id ? 
+    # TODO:try if None value works for db.Integer
