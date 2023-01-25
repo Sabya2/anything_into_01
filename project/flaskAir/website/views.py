7387795bde8f5b1b93a79652566e145eb5044f89
Lion Wolf <lionphilippwolf@gmail.com>
2023-01-25 09:47:17 +0100

    started working on the export feature for seat info

--- project/flaskAir/website/views.py
+++ project/flaskAir/website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
+from datetime import datetime
 from . import db
 import json
 
@@ -20,6 +21,21 @@ def home():
     return render_template("home.html", user=current_user)
 
 
+@views.route('/export2file', methods=['POST'])
+def export2file(all_seats, free_seats):
+    if current_user.is_admin:
+        with open('./seatinfo.txt', 'a') as seatinfo:
+            timeinfo = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            seatinfo.write(f"{timeinfo}:\t \
+                seats available: {free_seats}\t \
+                all seats: {all_seats}\t \
+                {(free_seats / all_seats) * 100} % available\n")
+            flash('sucessfully written seatinfo', category='success')
+        return render_template("admin.html", user=current_user)
+    else:
+        flash("This functionality is for admins only!", category='error')
+        return redirect(url_for('views.home'))
+
 
 ''' 
 TODO: Take this as an example how functionality in Flask can be constructed with jsonify
@@ -36,6 +52,8 @@ def delete_note():
 
     return jsonify({})
 '''
+
+
 @views.route('/help')
 def help():
     return render_template("help.html", user=current_user)
