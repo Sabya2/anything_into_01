e3e19787fa3f3a6a7c900a43c985633b36d2ef3f
Lion Wolf <lionphilippwolf@gmail.com>
2023-01-31 09:37:55 +0100

    added virtual environment
    with the virtual environment and the installation steps mentioned in readme, everybody should have the same working example of this project.

--- project/flaskAir/website/auth.py
+++ project/flaskAir/website/auth.py
@@ -38,8 +38,8 @@ def login():
         login_password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
-        if user: #checks if the user exists
-            if check_password_hash(user.password, login_password): #checks users password
+        if user:  # checks if the user exists
+            if check_password_hash(user.password, login_password):  # checks users password
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=False)
                 return redirect(url_for('views.home'))
@@ -53,7 +53,7 @@ def login():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST': # asking for the input information
+    if request.method == 'POST':  # asking for the input information
         email = request.form.get('email')
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
@@ -63,15 +63,15 @@ def signup():
         user = User.query.filter_by(email=email).first()  # retrieve users with this email
         if user:  # if email already registered
             flash('This email is already registered.', category='error')
-        elif len(email) < 4: # checking the lenght of the email
+        elif len(email) < 4: # checking the length of the email
             flash('email must be longer than 3 characters.', category='error')
-        elif len(firstname) < 2: # checking the lenght of the firstname
+        elif len(firstname) < 2: # checking the length of the firstname
             flash('First name must be longer than 1 character.', category='error')
-        elif len(lastname) < 2: # checking the lenght of the lastname
+        elif len(lastname) < 2:  # checking the length of the lastname
             flash('Last name must be longer than 1 character.', category='error')
-        elif len(password) < 7: # checking the lenght of the password
+        elif len(password) < 7:  # checking the length of the password
             flash('Passwords must be at least 7 characters.', category='error')
-        elif password != confirm_password: # checking the if the passwords match
+        elif password != confirm_password:  # checking the if the passwords match
             flash('Passwords do not match.', category='error')
         else:   # creates the new user and stores it in the database 
             new_user = User(email=email,
@@ -90,7 +90,7 @@ def signup():
 
 @auth.route('/logout')
 @login_required
-def logout(): #function to logout, using flask_login library
+def logout():  # to logout, using flask_login library
     flash('Logged out! Have a nice day :3', category='success')
     logout_user()
     return redirect(url_for('auth.login'))
